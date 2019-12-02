2d95d56e1d45b16d24d931b8e7ffc1f3488b5eaf
Leer0r <avedd72@gmail.com>
2019-12-02 22:40:25 +0100

    Ajout de la fonction pour lire les caractéristiques dans un fichier json

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
 import os,json,shutil
 
 class JSON_MASTER():
@@ -12,7 +15,6 @@ class JSON_MASTER():
     def nouvelle_personne(self,booleen):
         """
         Va créer un nouveau dossier de la forme <nom_prenom> avec un fichier data.json a l'interrieur
-
         @param : booleen -> True si on appelle la fonction en ayant le nom et le prenon, False sinon
 
         """
@@ -71,19 +73,44 @@ class JSON_MASTER():
     def cible(self):
         if self.verifier_current():
             print("Le repertoire courrant va etre changer")
-        self.current_familyname = input("Quel est le nom de la personne a définir comme reperoire courrant ? : ").lower()
-        self.current_name = input("Quel est le prenom de la personne a définir comme reperoire courrant ? : ").lower()
-        if not os.path.exists("data/" + self.current_familyname + "_" + self.current_name):
+        print("Liste des dossiers : ",end = "")
+        self.afficher_dossier_data_general()
+        tmp_current_familyname = input("Quel est le nom de la personne a définir comme reperoire courrant ? : ").lower()
+        print("Liste des choix : ",end = "")
+        self.afficher_dossier_data_specif(tmp_current_familyname)
+        tmp_current_name = input("Quel est le prenom de la personne a définir comme reperoire courrant ? : ").lower()
+        if not os.path.exists("data/" + tmp_current_familyname + "_" + tmp_current_name):
             choix = self.input_current("La personne n'a pas de dossier, voulez vous en créer un ? : (y pour valider)")
             if choix == "y":
+                self.current_name = tmp_current_name
+                self.current_familyname = tmp_current_familyname
                 self.nouvelle_personne(True)
             else:
                 print("Abandon")
                 return
+        else :
+            self.current_name = tmp_current_name
+            self.current_familyname = tmp_current_familyname
         self.current_path = "data/"+self.current_familyname + "_" + self.current_name 
         print("Le répertoire courrant est maintenant sur la personne nommée " + self.current_familyname + " " + self.current_name)
         
     #########################################################################################
+    ################################Lire les donnée de la cible##############################
+    
+    def lire_donnee(self):
+        if not self.verifier_current():
+            print("Veuiller cibler une personne avant cette action")
+            return
+        with open(self.current_path+"/data.json") as fichier:
+            self.current_json = json.load(fichier)
+            print("Les donnée ont bien été chargée \n")
+        for caract in self.current_json:
+            if self.current_json[caract] != "":
+                print(caract + " -> " + self.current_json[caract])
+        print()
+        
+
+    ##########################################################################################
 
     def verifier_current(self):
         if self.current_name == "" or self.current_familyname == "" or self.current_path == "":
@@ -102,24 +129,36 @@ class JSON_MASTER():
         else:
             choix = input("[Vide]" + " " + txt)
         return choix
-        
+    
+    def afficher_dossier_data_general(self):
+        for i in os.listdir("data/"):
+            if i != "template.json":
+                print(i,end="    ")
+        print()
 
+    def afficher_dossier_data_specif(self,nom):
+        for i in os.listdir("data/"):
+            if i != "template.json" and nom in i:
+                print(i,end="    ")
+        print()
 
 
 class MAIN(JSON_MASTER):
     def __init__(self):
         self.arret = False
         JSON_MASTER.__init__(self)
+        if not os.path.exists("data"):
+            os.mkdir("data")
 
     def decision(self,str_decision):
         if str_decision == "ajoute une nouvelle personne":
             self.nouvelle_personne(False)
-            return
-        if str_decision == "supprime cette personne":
+        elif str_decision == "supprime cette personne":
             self.supprimer_personne()
-            return
-        if str_decision == "cible une personne":
+        elif str_decision == "cible une personne":
             self.cible()
+        elif str_decision == "donne moi les informations":
+            self.lire_donnee()
         else:
             print("Désolé, je ne connais pas cette fonction....")
 
